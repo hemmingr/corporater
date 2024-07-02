e850278aef6808e61b2703f117a519e77c675082
Hemming Roaldsøy <90692985+hemmingr@users.noreply.github.com>
2024-07-02 11:37:55 +0200

    Update serve_files.py

--- serve_files.py
+++ serve_files.py
@@ -1,19 +1,19 @@
 import os
 
 def identify_user():
-    user_type = os.getenv('USER_TYPE', 'default')
-    return user_type
+    user_type = os.getenv('SERVER_ID', 'default')
+    return server_id
 
-def serve_files(user_type):
+def serve_files(server_id):
     files_to_serve = {
-        'user_type_1': 'user_type_1/file1.txt',
-        'user_type_2': 'user_type_2/file2.txt',
-        'user_type_3': 'user_type_3/file3.txt',
+        'server_dev': 'server_id/file1.txt',
+        'server_qa': 'server_id/file2.txt',
+        'prod': 'server_id/file3.txt',
         'default': 'default/file.txt'
     }
-    file_path = files_to_serve.get(user_type, files_to_serve['default'])
+    file_path = files_to_serve.get(server_id, files_to_serve['default'])
     print(f"Serving file: {file_path}")
 
 if __name__ == "__main__":
-    user_type = identify_user()
-    serve_files(user_type)
+    server_id = identify_user()
+    serve_files(server_id)
